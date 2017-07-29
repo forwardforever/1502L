b43f2c2ac2cbe85bd2d12201ba0c302c734fe64b
forwardforever <jnbfeng@163.com>
2017-07-29 10:50:49 +0800

    第二次提交

--- MvpDemo/app/src/main/java/com/bawei/mvp/MainActivity.java
+++ MvpDemo/app/src/main/java/com/bawei/mvp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次提交代码
+		//第二次提交代码
     }
 }
